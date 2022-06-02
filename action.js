abb34522e429154a8b2f0a44ad9074629a4c7b4b
Rashadtheone <rashad.hammonds0114@gmail.com>
2022-06-02 11:21:17 -0400

    functional calculator

--- action.js
+++ action.js
@@ -2,9 +2,34 @@
 //     console.log("hello")
 // }
 
-function entryNum() {
-    console.log("num button pushed")
-}
-function clearCal() {
-    console.log("Clear button pushed")
-}
+let display = document.getElementById('calAnswer')
+
+let buttons = Array.from(document.getElementsByClassName('numEntry'))
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+
+        switch(e.target.innerText){
+            case 'Clear':
+                display.innerText = " "
+                break;
+            case '←':
+                if(display.innerText) {
+                    display.innerText = display.innerText.slice(0,-1)
+                }
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = 'error!'
+                }
+                
+                break;
+
+            default:
+                display.innerText += e.target.innerText;
+        }
+
+    })
+})
