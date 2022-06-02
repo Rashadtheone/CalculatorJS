d6f0efce3bd2a2b925e4335482be613ac361899d
Rashadtheone <rashad.hammonds0114@gmail.com>
2022-06-02 09:12:40 -0400

    buttons added, test script added, basic css added

---
+++ action.js
@@ -0,0 +1,10 @@
+// function hello() {
+//     console.log("hello")
+// }
+
+function entryNum() {
+    console.log("num button pushed")
+}
+function clearCal() {
+    console.log("Clear button pushed")
+}
